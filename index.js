57f87e6ef04a1a14422aab7d19a35ee17ba92eb1
Dmitry <df.creative@gmail.com>
2017-09-30 23:33:52 -0400

    Init

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * @module update-diff
+ */
+
+'use strict'
+
+var cache = new WeakMap;
+
+module.exports = function updateDiff (obj, diff, mappers) {
+	var raw = cache.get(obj) || {}
+
+	cache.set(obj, diff)
+
+	if (!Array.isArray(mappers)) mappers = [].slice.call(arguments, 2)
+
+	for (var i = 0, l = mappers.length; i < l; i++) {
+		var dict = mappers[i]
+		for (var prop in dict) {
+			if (prop in raw && (raw[prop] === diff[prop] || obj[prop] === diff[prop])) continue
+
+			if (prop in diff) {
+				var result
+
+				if (dict[prop] === true) result = diff[prop]
+				else if (dict[prop] === false) continue
+				else if (typeof dict[prop] === 'function') {
+					result = dict[prop](diff[prop], diff, obj)
+					if (result === undefined) continue
+				}
+
+				obj[prop] = result
+			}
+		}
+	}
+
+	return obj
+}
